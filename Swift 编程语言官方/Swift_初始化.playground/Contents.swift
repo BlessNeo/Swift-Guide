0c9d435c0c7523ff6acbf70b791dfee493453b32
Neo_ZH <luckyyafei@163.com>
2018-10-23 14:19:01 +0800

    继续学习初始化

--- Swift 编程语言官方/Swift_初始化.playground/Contents.swift
+++ Swift 编程语言官方/Swift_初始化.playground/Contents.swift
@@ -229,3 +229,169 @@ let centerRect = Rect(center: Point(x: 4.0, y: 4.0),
 //Swift 的两段式初始化过程与 Objective-C 的初始化类似。主要的不同点是在第一阶段，Objective-C 为每一个属性分配零或空值(例如 0 或 nil )。Swift 的初始化流程更加灵活，它允许你设置自定义的初始值，并可以自如应对那些不把 0 或 nil 做为合法值的类型。
 //Swift编译器执行四种有效的安全检查来确保两段式初始化过程能够顺利完成：
 
+//安全检查 1
+
+//指定初始化器必须保证在向上委托给父类初始化器之前，其所在类引入的所有属性都要初始化完成。
+
+//如上所述，一个对象的内存只有在其所有储存型属性确定之后才能完全初始化。为了满足这一规则，指定初始化器必须保证它自己的属性在它上交委托之前先完成初始化。
+
+//安全检查 2
+
+//指定初始化器必须先向上委托父类初始化器，然后才能为继承的属性设置新值。如果不这样做，指定初始化器赋予的新值将被父类中的初始化器所覆盖。
+
+//安全检查 3
+
+//便捷初始化器必须先委托同类中的其它初始化器，然后再为任意属性赋新值（包括同类里定义的属性）。如果没这么做，便捷构初始化器赋予的新值将被自己类中其它指定初始化器所覆盖。
+
+//安全检查 4
+
+//初始化器在第一阶段初始化完成之前，不能调用任何实例方法、不能读取任何实例属性的值，也不能引用 self 作为值。
+
+//直到第一阶段结束类实例才完全合法。属性只能被读取，方法也只能被调用，直到第一阶段结束的时候，这个类实例才被看做是合法的。
+//以下是两段初始化过程，基于上述四种检查的流程：
+//阶段 1
+
+//指定或便捷初始化器在类中被调用；
+//为这个类的新实例分配内存。内存还没有被初始化；
+//这个类的指定初始化器确保所有由此类引入的存储属性都有一个值。现在这些存储属性的内存被初始化了；
+//指定初始化器上交父类的初始化器为其存储属性执行相同的任务；
+//这个调用父类初始化器的过程将沿着初始化器链一直向上进行，直到到达初始化器链的最顶部；
+//一旦达了初始化器链的最顶部，在链顶部的类确保所有的存储属性都有一个值，此实例的内存被认为完全初始化了，此时第一阶段完成。
+//阶段 2
+
+//从顶部初始化器往下，链中的每一个指定初始化器都有机会进一步定制实例。初始化器现在能够访问 self 并且可以修改它的属性，调用它的实例方法等等；
+//最终，链中任何便捷初始化器都有机会定制实例以及使用 slef 。
+
+//初始化器的继承和重写
+//不像在 Objective-C 中的子类，Swift 的子类不会默认继承父类的初始化器。Swift 的这种机制防止父类的简单初始化器被一个更专用的子类继承并被用来创建一个没有完全或错误初始化的新实例的情况发生。
+//只有在特定情况下才会继承父类的初始化器，但只有这样是安全且合适的时候。
+//在子类的初始化器定义之前你必须写 override 修饰符。如同默认初始化器所描述的那样，即使是自动提供的默认初始化器你也可以重写。
+//作为一个重写的属性，方法或下标脚本， override 修饰符的出现会让 Swift 来检查父类是否有一个匹配的指定初始化器来重写，并且验证你重写的初始化器已经按照意图指定了形式参数。
+//当重写父类指定初始化器时，你必须写 override 修饰符，就算你子类初始化器的实现是一个便捷初始化器
+//相反，如同上边类类型的初始化器委托所描述的规则那样，如果你写了一个匹配父类便捷初始化器的子类初始化器，父类的便捷初始化器将永远不会通过你的子类直接调用。因此，你的子类不能(严格来讲)提供父类初始化器的重写。当提供一个匹配的父类便捷初始化器的实现时，你不用写 override 修饰符。
+
+class Vehicle {
+    var numberOfWheels = 0
+    var description: String {
+        return "\(numberOfWheels) wheel(s)"
+    }
+}
+//Vehicle 类只为它的存储属性提供了默认值，并且没有提供自定义的初始化器。因此，如同默认初始化器中描述的那样，它会自动收到一个默认初始化器。默认初始化器(如果可用的话)总是类的指定初始化器，也可以用来创建一个新的 Vehicle 实例， numberOfWheels 默认为 0
+let vehicle = Vehicle()
+print("Vehicle:\(vehicle.description)")
+
+class Bicycle: Vehicle {
+    override init() {
+        super.init()
+        numberOfWheels = 2
+    }
+}
+
+//子类 Bicycle 定义了一个自定义初始化器 init() 。这个指定初始化器和 Bicycle 的父类的指定初始化器相匹配，所以 Bicycle 中的指定初始化器需要带上 override 修饰符。
+//Bicycle 类的 init() 初始化器以调用 super.init() 开始，这个方法作用是调用父类的初始化器。这样可以确保 Bicycle 在修改属性之前它所继承的属性 numberOfWheels 能被 Vehicle 类初始化。在调用 super.init() 之后，一开始的 numberOfWheels 值会被新值 2 替换。
+
+let bicycle = Bicycle()
+print("Bicycle: \(bicycle.description)")
+//子类可以在初始化时修改继承的变量属性，但是不能修改继承过来的常量属性。
+//初始化器的自动继承
+//如上所述，子类默认不会继承父类初始化器。总之，在特定的情况下父类初始化器是可以被自动继承的。实际上，这意味着在许多场景中你不必重写父类初始化器，只要可以安全操作，你就可以毫不费力地继承父类的初始化器。
+//假设你为你子类引入的任何新的属性都提供了默认值，请遵守以下2个规则：
+//规则1
+
+//如果你的子类没有定义任何指定初始化器，它会自动继承父类所有的指定初始化器。
+
+//规则2
+
+//如果你的子类提供了所有父类指定初始化器的实现——要么是通过规则1继承来的，要么通过在定义中提供自定义实现的——那么它自动继承所有的父类便捷初始化器。
+
+//就算你的子类添加了更多的便捷初始化器，这些规则仍然适用。
+//子类能够以便捷初始化器的形式实现父类指定初始化器来作为满足规则2的一部分
+
+class Food {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    convenience init() {
+        self.init(name: "[Unnamed]")
+    }
+}
+
+let namedMeat = Food(name: "Bacon")
+print("\(namedMeat.name)")
+
+class RecipeIngredient: Food {
+    var quantity: Int
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
+    }
+}
+
+let oneMysteryItem = RecipeIngredient()
+let oneBacon = RecipeIngredient(name: "Bacon")
+let sixEggs = RecipeIngredient(name: "Eggs", quantity: 6)
+
+class ShoppingListItem0: RecipeIngredient {
+    var purchased = false
+    var description: String {
+        var output = "\(quantity) x \(name)"
+        output += purchased ? "✔" : "✘"
+        return output
+    }
+}
+
+//ShoppingListItem 没有定义初始化器来给 purchased 一个初始值，这是因为任何添加到购物单（这里的模型）的项的初始状态总是未购买
+//由于它为自己引入的所有属性提供了一个默认值并且自己没有定义任何初始化器， ShoppingListItem 会自动从父类继承所有的指定和便捷初始化器
+
+var breakfastList = [
+    ShoppingListItem0(),
+    ShoppingListItem0(name: "Bacon"),
+    ShoppingListItem0(name: "Eggs", quantity: 6)
+]
+breakfastList[0].name = "Orange juice"
+breakfastList[1].purchased = true
+for item in breakfastList {
+    print(item.description)
+}
+//可失败初始化器
+//定义类、结构体或枚举初始化时可以失败在某些情况下会管大用。这个失败可能由以下几种方式触发，包括给初始化传入无效的形式参数值，或缺少某种外部所需的资源，又或是其他阻止初始化的情况。
+
+//为了妥善处理这种可能失败的情况，在类、结构体或枚举中定义一个或多个可失败的初始化器。通过在 init 关键字后面添加问号( init? )来写
+//你不能定义可失败和非可失败的初始化器为相同的形式参数类型和名称。
+//可失败的初始化器创建了一个初始化类型的可选值。你通过在可失败初始化器写 return nil 语句，来表明可失败初始化器在何种情况下会触发初始化失败。
+//严格来讲，初始化器不会有返回值。相反，它们的角色是确保在初始化结束时， self 能够被正确初始化。虽然你写了 return nil 来触发初始化失败，但是你不能使用 return 关键字来表示初始化成功了
+//比如说，可失败初始化器为数字类型转换器做实现。为了确保数字类型之间的转换保持值不变，使用 init(exactly:)  初始化器。如果类型转换不能保持值不变，初始化器失败
+let wholeNumner: Double = 12345.0
+let pi = 3.14159
+if let valueMaintained = Int(exactly: wholeNumner) {
+    print("\(wholeNumner) conversion to int maintains value")
+}
+
+let valueChanged = Int(exactly: pi)
+if valueChanged == nil {
+    print("\(pi) conversion to int does not maintain value")
+}
+
+struct Animal {
+    let species: String
+    init?(species: String) {
+        if species.isEmpty{ return nil }
+        self.species = species
+    }
+}
+
+let someCreature = Animal(species: "Giraffe")
+if let giraffe = someCreature {
+    print("An animal was initialized with a species of \(giraffe.species)")
+}
+
+let anonymousCreature = Animal(species: "")
+if anonymousCreature == nil {
+    print("The anonymous creature could not be initialized")
+}
+//检查空字符串值(比如是 "" 而不是 "Giraffe" )和检查值是否为 nil 来表明可选项String是不是没有值是两个不一样的的概念。上面的栗子中，空字符串( "" )是合法的，非可选的 String 。总之，对于 Animal 来说让它的species属性有一个空的字符串作为值是不合适的。要模式化这个限制，可失败的初始化器就会在发现空字符串时触发初始化失败
+//枚举的可失败初始化器
