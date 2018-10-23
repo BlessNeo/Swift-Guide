2b83597eab03e441dab26779604e190fb04b6c6c
Neo_ZH <luckyyafei@163.com>
2018-10-23 15:40:29 +0800

    代码示例

--- Swift 编程语言官方/Swift_反初始化.playground/Contents.swift
+++ Swift 编程语言官方/Swift_反初始化.playground/Contents.swift
@@ -8,3 +8,44 @@ import UIKit
 //deinit {
 //    // perform the deinitialization
 //}
+
+//反初始化器会在实例被释放之前自动被调用。你不能自行调用反初始化器。父类的反初始化器可以被子类继承，并且子类的反初始化器实现结束之后父类的反初始化器会被调用。父类的反初始化器总会被调用，就算子类没有反初始化器。
+
+//由于实例在反初始化器被调用之前都不会被释放，反初始化器可以访问实例中的所有属性并且可以基于这些属性修改自身行为（比如说查找需要被关闭的那个文件的文件名）。
+//应用反初始化器
+
+class Bank{
+    static var coinsInBank = 10_000
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+        coinsInBank -= numberOfCoinsToVend;
+        return numberOfCoinsToVend
+    }
+    static func receive(coins: Int) {
+        coinsInBank += coins
+    }
+}
+
+class Player {
+    var coinsInPurse: Int
+    init(coins: Int) {
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins: Int)  {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+var playerOne: Player? = Player(coins: 100)
+print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+print("There are now \(Bank.coinsInBank) coins left in the bank")
+
+playerOne!.win(coins: 2_000)
+print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+print("There are now \(Bank.coinsInBank) coins left in the bank")
+playerOne = nil
+print("PlayerOne has left the game")
+print("There are now \(Bank.coinsInBank) coins left in the bank")
