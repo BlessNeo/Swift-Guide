b5c29ca0272c90e76c61baf4e29bea2833737020
Neo_ZH <luckyyafei@163.com>
2018-10-31 13:18:33 +0800

    simple stop watch

--- ProjectsOfSwift/Project 01 - SimpleStopWatch/SimpleStopWatch/SimpleStopWatch/ViewController.swift
+++ ProjectsOfSwift/Project 01 - SimpleStopWatch/SimpleStopWatch/SimpleStopWatch/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnPause: UIButton!
+    @IBOutlet weak var lblTime: UILabel!
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isPlaying = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lblTime.text = String(counter)
     }
 
-
+    @IBAction func btnResetPressed(_ sender: Any) {
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        lblTime.text = String(counter)
+        btnPlay.isEnabled = true
+        btnPause.isEnabled = true
+    }
+    
+    @IBAction func btnPlayPressed(_ sender: Any) {
+        if isPlaying {
+            return
+        }
+        btnPlay.isEnabled = false
+        btnPause.isEnabled = true
+        timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                     target: self,
+                                     selector: #selector(self.updateTimer),
+                                     userInfo: nil,
+                                     repeats: true)
+        isPlaying = true
+    }
+    
+    @IBAction func btnPausePressed(_ sender: Any) {
+        btnPlay.isEnabled = true
+        btnPause.isEnabled = false
+        timer.invalidate()
+        isPlaying = false
+    }
+    //Swift 和 Objective-C 的互调
+    @objc func updateTimer()  {
+        counter += 0.1
+        lblTime.text = String(format: "%.1f", counter)
+    }
 }
 
--- ProjectsOfSwift/README.md
+++ ProjectsOfSwift/README.md
@@ -2,4 +2,7 @@
 
 ### 学习 [30DaysofSwift](https://github.com/allenwong/30DaysofSwift) 等类似的项目，在实践中成长。
 
+#### Project 01 - SimpleStopWatch 
+一个简单的秒表
+
 
