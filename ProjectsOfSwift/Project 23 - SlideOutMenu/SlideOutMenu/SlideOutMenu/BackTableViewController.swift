5aa82144d445b4283ba84682777befcb90f3dd77
BlessNeo <luckyyafei@163.com>
2019-06-16 10:25:06 +0800

    Project 23 - SlideOutMenu
    Objective-C 与 Swift 混编，添加一个桥接头文件

---
+++ ProjectsOfSwift/Project 23 - SlideOutMenu/SlideOutMenu/SlideOutMenu/BackTableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  BackTableViewController.swift
+//  SlideOutMenu
+//
+//  Created by BlessNeo on 2019/6/16.
+//  Copyright © 2019 NeoZH. All rights reserved.
+//
+
+import UIKit
+
+class BackTableViewController: UITableViewController {
+
+    var TableArrary = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        TableArrary = ["FriendRead", "Article", "ReadLater"]
+        self.tableView.tableFooterView = UIView(frame: .zero)
+        self.tableView.separatorColor = UIColor(red:0.159, green:0.156, blue:0.181, alpha:1)
+        self.view.backgroundColor = UIColor.black
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return TableArrary.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableArrary[indexPath.row], for: indexPath)
+        cell.textLabel?.text = TableArrary[indexPath.row]
+        cell.backgroundColor = .clear
+        cell.textLabel?.textColor = .white
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
+        selectedCell.contentView.backgroundColor = UIColor(red:0.245, green:0.247, blue:0.272, alpha:0.817)
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProjectsOfSwift/Project 23 - SlideOutMenu/SlideOutMenu/SlideOutMenu/FriendReadViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FriendReadViewController.swift
+//  SlideOutMenu
+//
+//  Created by BlessNeo on 2019/6/16.
+//  Copyright © 2019 NeoZH. All rights reserved.
+//
+
+import UIKit
+
+class FriendReadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.isNavigationBarHidden = true
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
