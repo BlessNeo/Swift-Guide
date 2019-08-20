9e58e57ec9ce10b2ed644d1422701fd1baf2d290
ZhangYafei <Zhang_YaFei@want-want.com>
2019-08-20 21:44:12 +0800

    coredata

--- ProjectsOfSwift/Project 26 - CoreDataApp/CoreDataDemo/CoreDataDemo/AppDelegate.swift
+++ ProjectsOfSwift/Project 26 - CoreDataApp/CoreDataDemo/CoreDataDemo/AppDelegate.swift
@@ -45,6 +45,64 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: - Core Data stack
+    
+    /*
+     定义一个懒加载属性最简单的办法就是在 var 前加一个 lazy 关键字,
+     并且还要给 出默认的值。这个默认值会在这个属性被第一次访问的时候被指定,
+     也就是说在这个对象的初始化方法中这个就不需要再去初始化这个属性了。
+     */
+    /*
+     使用自执行闭包
+     
+    除了使用工厂方法来返回这个属性值以外,你也可以选择在这个属性声明的地方通过一个
+     自执行的闭包来初始化这个属性。
+     这样做有一个好处: 让这个属性的在同一个地方声明和设置。当然阅读这些代码可能会比较不爽。特别是在这个属性的设置需要很长的代码的时候。我自己的规则是当这个属性的初始化方法只有两三行代码的时候。就是使用这样的方法来初始化懒加载属性。
+     */
+    
+    lazy var applicationDocumentsDirectory : URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "me.appkitchen.cd" in the application's documents Application Support directory.
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[urls.count - 1]
+    }()
+    
+    lazy var managedObjectModel : NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelUrl = Bundle.main.url(forResource: "CoreDataDemo", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelUrl)!
+    }()
+    
+    lazy var persistentStoreCoordinator : NSPersistentStoreCoordinator = {
+        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.appendingPathComponent("SingleViewCoreData.sqlite")
+        var failureReason = "There was an error creating or loading the application's saved data."
+        do {
+            try
+            coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
+        } catch {
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject
+            dict[NSLocalizedFailureErrorKey] = failureReason as AnyObject
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+        return coordinator
+    }()
+    
+    lazy var managedObjectContext : NSManagedObjectContext = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
 
     lazy var persistentContainer: NSPersistentContainer = {
         /*
@@ -76,10 +134,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data Saving support
 
     func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
+        //let context = persistentContainer.viewContext
+        if managedObjectContext.hasChanges {
             do {
-                try context.save()
+                try managedObjectContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
--- ProjectsOfSwift/Project 26 - CoreDataApp/CoreDataDemo/CoreDataDemo/ViewController.swift
+++ ProjectsOfSwift/Project 26 - CoreDataApp/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -7,12 +7,106 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var listItems = [NSManagedObject]()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.addItem))
+        self.tableView.tableFooterView = UIView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContex = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ListEntity")
+        
+        do {
+            let result = try managedContex.fetch(fetchRequest)
+            listItems = result as! [NSManagedObject]
+            self.tableView.reloadData()
+        } catch  {
+            print("error")
+        }
+        
+    }
+    
+    @objc func addItem() {
+        let alertCtrl = UIAlertController(title: "这是标题", message: "", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "确定", style: .default) { (_) in
+            if let field = alertCtrl.textFields![0] as? UITextField {
+                self.saveItem(itemToSave: field.text!)
+                self.tableView.reloadData()
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        alertCtrl.addTextField { (textField) in
+            textField.placeholder = "输入点儿啥吧"
+        }
+        alertCtrl.addAction(confirmAction)
+        alertCtrl.addAction(cancelAction)
+        self.present(alertCtrl, animated: true, completion: nil)
+    }
+    
+    func saveItem(itemToSave: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContex = appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entity(forEntityName: "ListEntity", in: managedContex)
+        let item = NSManagedObject(entity: entity!, insertInto: managedContex)
+        item.setValue(itemToSave, forKey: "item")
+        do {
+            try managedContex.save()
+            listItems.append(item)
+        } catch  {
+            print("error")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        let item = listItems[indexPath.row]
+        cell.textLabel?.text = item.value(forKey: "item") as? String
+        cell.textLabel?.font = UIFont(name: "", size: 25)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .normal, title: "删除") { (action, index) in
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let managedContex = appDelegate.managedObjectContext
+            tableView.reloadRows(at: [indexPath], with: .right)
+            managedContex.delete(self.listItems[indexPath.row])
+            
+            do {
+                try managedContex.save()
+                self.listItems.remove(at: indexPath.row)
+                self.tableView.reloadData()
+            } catch {
+                print("error: delete")
+            }
+        }
+        
+        delete.backgroundColor = UIColor.red
+        
+        return [delete]
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
 
 
