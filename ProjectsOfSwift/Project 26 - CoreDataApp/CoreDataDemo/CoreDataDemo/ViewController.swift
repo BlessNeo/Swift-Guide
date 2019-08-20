71025dff9f57476f7a3bafaf3449d804f74437df
ZhangYafei <Zhang_YaFei@want-want.com>
2019-08-20 15:31:49 +0800

    创建工程

---
+++ ProjectsOfSwift/Project 26 - CoreDataApp/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by BlessNeo on 2019/8/20.
+//  Copyright © 2019 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
