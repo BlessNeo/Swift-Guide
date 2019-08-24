33dfff1c418bbc05fe80750115ba0d4fb798ea36
ZhangYafei <Zhang_YaFei@want-want.com>
2019-08-24 21:14:47 +0800

    init spotlight

---
+++ ProjectsOfSwift/Project 28 - SpotlightSearch/SpotLight/SpotLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotLight
+//
+//  Created by BlessNeo on 2019/8/24.
+//  Copyright © 2019 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
